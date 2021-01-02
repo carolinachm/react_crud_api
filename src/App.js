e05182029f241ec4d4b9d335d31a2626dcb86d08
carolina mesquita <carolti2013@gmail.com>
2021-01-02 18:50:00 -0300

    criando a tela de cadastro de usuario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Login from './views/login'
+import Usuario from './views/usuario'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
 import './custom.css'
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Login />
+        <Usuario />
       
 
       </div>
